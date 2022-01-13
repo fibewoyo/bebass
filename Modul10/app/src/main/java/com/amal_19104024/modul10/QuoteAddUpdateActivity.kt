e8a89a772879029cf13a1b354cf0fa050e69e1b6
fibewoyo <94056341+fibewoyo@users.noreply.github.com>
2022-01-13 13:15:42 +0700

    Update QuoteAddUpdateActivity.kt

--- Modul10/app/src/main/java/com/amal_19104024/modul10/QuoteAddUpdateActivity.kt
+++ Modul10/app/src/main/java/com/amal_19104024/modul10/QuoteAddUpdateActivity.kt
@@ -1,4 +1,4 @@
-package com.rifqi_19104031.modul10
+package com.amal_19104024.modul10
 
 import android.R
 import android.content.ContentValues
@@ -12,19 +12,19 @@ import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
-import com.rifqi_19104031.modul10.data.Quote
-import com.rifqi_19104031.modul10.db.DatabaseContract
-import com.rifqi_19104031.modul10.db.DatabaseContract.QuoteColumns.Companion.DATE
-import com.rifqi_19104031.modul10.db.QuoteHelper
-import com.rifqi_19104031.modul10.helper.ALERT_DIALOG_CLOSE
-import com.rifqi_19104031.modul10.helper.ALERT_DIALOG_DELETE
-import com.rifqi_19104031.modul10.helper.EXTRA_POSITION
-import com.rifqi_19104031.modul10.helper.EXTRA_QUOTE
-import com.rifqi_19104031.modul10.helper.RESULT_ADD
-import com.rifqi_19104031.modul10.helper.RESULT_DELETE
-import com.rifqi_19104031.modul10.helper.RESULT_UPDATE
-import com.rifqi_19104031.modul10.helper.categoryList
-import com.rifqi_19104031.modul10.helper.getCurrentDate
+import com.amal_19104024.modul10.data.Quote
+import com.amal_19104024.modul10.db.DatabaseContract
+import com.amal_19104024.modul10.db.DatabaseContract.QuoteColumns.Companion.DATE
+import com.amal_19104024.modul10.db.QuoteHelper
+import com.amal_19104024.modul10.helper.ALERT_DIALOG_CLOSE
+import com.amal_19104024.modul10.helper.ALERT_DIALOG_DELETE
+import com.amal_19104024.modul10.helper.EXTRA_POSITION
+import com.amal_19104024.modul10.helper.EXTRA_QUOTE
+import com.amal_19104024.modul10.helper.RESULT_ADD
+import com.amal_19104024.modul10.helper.RESULT_DELETE
+import com.amal_19104024.modul10.helper.RESULT_UPDATE
+import com.amal_19104024.modul10.helper.categoryList
+import com.amal_19104024.modul10.helper.getCurrentDate
 
 class QuoteAddUpdateActivity : AppCompatActivity() {
     private var isEdit = false
